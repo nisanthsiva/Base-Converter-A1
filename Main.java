631cab5126251a49f293dc938d2b08e664acef87
Nisanth S <nisanthsk8@gmail.com>
2023-09-19 19:07:18 -0400

    commit

--- Main.java
+++ Main.java
@@ -1,43 +1,44 @@
 public class Main {
   public static void main(String[] args) {
-    //binaryToDecimal(1100);
-    //System.out.println(50 - 16);
-    decimalToBinary(17);
+    //System.out.println(binaryToDecimal(1100));
+    //System.out.println(decimalToBinary(12));
   }
 
   public static void converter(int N, int base1, int base2) {
 
   }
 
-  public static int sum = 0;
-  public static void binaryToDecimal(int bin) {
+  public static int binaryToDecimal(int bin) {
+    int sum = 0;
     String binString = String.format("%d",bin);
     int pow = 0;
     for(int i = binString.length()-1; i >= 0; i--) {
-      //System.out.println(binString.charAt(i));
       if(binString.charAt(i) == '1') {
         sum += Math.pow(2, pow);
-      } else;
+      }
       pow++;
     }
-    System.out.println(sum);
+    return sum;
   }
 
-  public static void decimalToBinary(int dec) {
-  String output = "";
-  int pow = 0;
-  while(Math.pow(2, pow) < dec) {
-    pow++;
-  }
-  while(pow > 0) {
-    if(dec-Math.pow(2, pow) < 0) {
-      pow--;
-      output += '0';
-    } else {
-      dec -= Math.pow(2, pow);
-      output += '1';
+  public static String decimalToBinary(int num) {
+    String result = "";
+    String output = "";
+    int remainder = 0;
+    while(num > 0) {
+      remainder = num % 2;
+      if(remainder == 0) {
+        result += '0';
+      }
+      else if(remainder == 1) {
+        result += '1';
+      }
+      num /= 2;
     }
-  }
-  System.out.println(output);
+    for(int i = result.length()-1; i >= 0; i--) {
+      output += result.charAt(i);
+    }
+    return output;
   }
 }
+
