2f63d7c0cf092372e117b90b33863409e3a936be
Nisanth S <nisanthsk8@gmail.com>
2023-09-21 21:54:19 -0400

    final uncommented

--- Main.java
+++ Main.java
@@ -1,61 +1,31 @@
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
-    //test:
-    //System.out.println(baseNToDecimal(1010, 2));
-    //System.out.println(decimalToBase(6, 2));
-
-    //System.out.println('A' - 55);
-    //System.out.println('A' - '0');
-    //System.out.println(baseNToDecimal("ZZ", 36));
-
-    //System.out.println(decimalToBaseN(baseNToDecimal(1100, 2), 2));
-    //System.out.println(decimalToHexadecimal(20, 32));
-    //System.out.println(decimalToBaseN(4, 4));
-    
-    //int testInt = 10;
-    //char testChar = (char)(testInt + 55);
-    //System.out.println(testChar);
+    Scanner scan = new Scanner(System.in);
+    System.out.println("What Number Do You Want To Convert?");
+    int number = scan.nextInt();
+    System.out.println("Which Base Do You Want To Convert From?");
+    int startingBase = scan.nextInt();
+    System.out.println("Which Base Do You Want To Convert To?");
+    int endingBase = scan.nextInt();
+    System.out.println(converter(number, startingBase, endingBase));
+    scan.close();
   }
 
-  public static void converter(int N, int base1, int base2) {
-
+  public static String converter(int N, int base1, int base2) {
+    return decimalToHexadecimal(hexaToDecimal(N, base1), base2);
   }
-
-  public static int baseNToDecimal(int num, int base1) {
-    int sum = 0;
-    String binString = String.format("%d", num);
-    int pow = 0;
-    for(int i = binString.length()-1; i >= 0; i--) {
-      char temp = binString.charAt(i);
-      sum += (temp - '0') * Math.pow(base1, pow);
-      pow++;
-    }
-    return sum;
-  }
-
-  public static String decimalToBaseN(int num, int base2) {
-    String result = "";
-    String output = "";
-    int remainder = 0;
-    while(num > 0) {
-      remainder = num % base2;
-      String remainderString = String.format("%d", remainder);
-      result += remainderString;
-      num /= base2;
-    }
-    for(int i = result.length()-1; i >= 0; i--) {
-      output += result.charAt(i);
-    }
-    return output;
-  }
-
-  public static int baseNToDecimal(String num, int base1) {
+  
+  public static int hexaToDecimal(int N, int base1) {
+    String num = String.format("%d", N);
     int sum = 0;
     int pow = 0;
     for(int i = num.length()-1; i >= 0; i--) {
       char temp = num.charAt(i);
       if(temp - 55 < 0) {
-        sum += (temp = '0') * Math.pow(base1, pow);
+        //sum += (temp = '0') * Math.pow(base1, pow);
+        sum += (temp - '0') * Math.pow(base1, pow);
       }
       else {
         sum += (temp - 55) * Math.pow(base1, pow);
